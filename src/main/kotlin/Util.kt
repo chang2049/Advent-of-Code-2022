a45091219746467a16690d5aea1433f36f21ceac
chang <cliu@linecorp.com>
2022-12-02 00:01:48 +0900

    Clean up util

--- src/main/kotlin/Util.kt
+++ src/main/kotlin/Util.kt
@@ -1,7 +1,3 @@
-fun main() {
-    val content = "day01.txt".loadFile().split("\n\n")[0]
-    println(content)
-}
 fun <T : Any>String.asResource(work: (String) -> T): T {
     val content = this.loadFile()
     return work(content)
