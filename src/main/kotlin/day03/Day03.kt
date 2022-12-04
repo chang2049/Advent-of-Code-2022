7070199421fbc0f23fc3683956b70902f0c12070
chang <cliu@linecorp.com>
2022-12-04 14:46:32 +0900

    Fix ktlint errors

--- src/main/kotlin/day03/Day03.kt
+++ src/main/kotlin/day03/Day03.kt
@@ -1,22 +1,24 @@
 package day03
 
 import asResource
-fun main(){
+fun main() {
     val items = processFile()
     println("Q1: ${sumPriorityValues(items)}")
     println("Q2: ${sumBadgeItemValues(items)}")
 }
 
 fun sumPriorityValues(items: List<String>) = items
-    .map { Pair(it.substring(0,it.length/2), it.substring(it.length/2,it.length))
-        .findDuplicatedChar().getValue() }.sum()
+    .map {
+        Pair(it.substring(0, it.length / 2), it.substring(it.length / 2, it.length))
+            .findDuplicatedChar().getValue()
+    }.sum()
 
 fun sumBadgeItemValues(items: List<String>) = (items.indices step 3).map {
-    items[it].toSet().intersect(items[it+1].toSet()).intersect(items[it+2].toSet()).toList()[0].getValue()
+    items[it].toSet().intersect(items[it + 1].toSet()).intersect(items[it + 2].toSet()).toList()[0].getValue()
 }.sum()
 
-fun Pair<String,String>.findDuplicatedChar() = this.first.toSet().first{this.second.toSet().contains(it)}
+fun Pair<String, String>.findDuplicatedChar() = this.first.toSet().first { this.second.toSet().contains(it) }
 
-fun Char.getValue() = if (this.isUpperCase()) this-'A'+27 else this-'a'+1
+fun Char.getValue() = if (this.isUpperCase()) this - 'A' + 27 else this - 'a' + 1
 
 fun processFile() = "day03.txt".asResource { it.split("\n") }
--- src/main/kotlin/day04/Day04.kt
+++ src/main/kotlin/day04/Day04.kt
@@ -2,27 +2,28 @@ package day04
 
 import asResource
 
-fun main(){
+fun main() {
     val pairs = processFile()
     println("Q1: ${countPairs(pairs, isFullyContained)}")
     println("Q2: ${countPairs(pairs, isOverlapped)}")
 }
 
 fun countPairs(
-    pairs:  List<Pair<Pair<Int, Int>, Pair<Int, Int>>>,
-    validate:(Pair<Int, Int>, Pair<Int, Int>) -> Boolean) =
+    pairs: List<Pair<Pair<Int, Int>, Pair<Int, Int>>>,
+    validate: (Pair<Int, Int>, Pair<Int, Int>) -> Boolean
+) =
     pairs.count { validate(it.first, it.second) }
 
-val isFullyContained:(Pair<Int, Int>, Pair<Int, Int>)->Boolean = {
-    sectionA: Pair<Int, Int>, sectionB: Pair<Int, Int> ->
+val isFullyContained: (Pair<Int, Int>, Pair<Int, Int>) -> Boolean = {
+        sectionA: Pair<Int, Int>, sectionB: Pair<Int, Int> ->
     (sectionA.first <= sectionB.first && sectionA.second >= sectionB.second) ||
-            (sectionA.first >= sectionB.first && sectionA.second <= sectionB.second)
+        (sectionA.first >= sectionB.first && sectionA.second <= sectionB.second)
 }
 
-val isOverlapped:(Pair<Int, Int>, Pair<Int, Int>)->Boolean = {
+val isOverlapped: (Pair<Int, Int>, Pair<Int, Int>) -> Boolean = {
         sectionA: Pair<Int, Int>, sectionB: Pair<Int, Int> ->
-    (sectionA.first <= sectionB.second && sectionA.second>=sectionB.first)||
-            (sectionB.first <= sectionA.second && sectionB.second>=sectionA.first)
+    (sectionA.first <= sectionB.second && sectionA.second >= sectionB.first) ||
+        (sectionB.first <= sectionA.second && sectionB.second >= sectionA.first)
 }
 
 fun processFile() = "day04.txt".asResource {
@@ -32,4 +33,4 @@ fun processFile() = "day04.txt".asResource {
     }
 }
 
-fun String.parseSection() = Pair(this.split("-")[0].toInt(),this.split("-")[1].toInt())
+fun String.parseSection() = Pair(this.split("-")[0].toInt(), this.split("-")[1].toInt())
